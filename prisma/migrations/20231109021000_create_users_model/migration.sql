1db6b7b899ca38e8a2aaac25c535e051738e1737
damattag <guilherme.damatta@yahoo.com.br>
2023-11-08 23:10:17 -0300

    setup

---
+++ prisma/migrations/20231109021000_create_users_model/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "accepted_terms" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+
+import './env';
+import app from "./app";
+import './database';
+
+
+app.listen({
+  port: process.env.SERVER_PORT,
+}, () => {
+  console.log(`🚀 Server ready at http://localhost:${process.env.SERVER_PORT}`);
+})
